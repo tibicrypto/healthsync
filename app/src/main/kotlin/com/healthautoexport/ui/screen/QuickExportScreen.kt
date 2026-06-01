5ae1e43fc04f4c0cc9470c8b6c58e940dd972b93
DESI MARIA PAKPAHAN <dmpakpahan025@outlook.com>
2026-06-01 11:42:48 +0000

    up

--- app/src/main/kotlin/com/healthautoexport/ui/screen/QuickExportScreen.kt
+++ app/src/main/kotlin/com/healthautoexport/ui/screen/QuickExportScreen.kt
@@ -16,6 +16,7 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
